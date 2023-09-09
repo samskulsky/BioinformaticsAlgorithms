0d740bfd454750e045daa83374b6e2a831033f22
Sam Skulsky <sam_skulsky@icloud.com>
2023-09-09 15:15:58 -0700

    frequencyTable and maxMap

--- src/HiddenMessages.java
+++ src/HiddenMessages.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class HiddenMessages {
 
     // Finds the number of times a pattern appears in a string
@@ -11,6 +14,40 @@ public class HiddenMessages {
         }
 
         return count;
+    }
+
+    // Maps all k-mers in a string to the number of times they appear
+    public static Map<String, Integer> frequencyTable(String text, int k) {
+        Map<String, Integer> freqMap = new HashMap<>();
+
+        int n = text.length();
+
+        for (int i = 0; i < n - k; i++) {
+            String pattern = text.substring(i, i + k);
+
+            if (!freqMap.containsKey(pattern)) {
+                freqMap.put(pattern, 1);
+            } else {
+                freqMap.put(pattern, freqMap.get(pattern) + 1);
+            }
+        }
+
+        return freqMap;
+    }
+
+    // Returns the maximum value in a map
+    public static int maxMap(Map<String, Integer> freqMap) {
+        int maxValue = 0;
+        boolean first = true;
+
+        for (int value : freqMap.values()) {
+            if (first || value > maxValue) {
+                maxValue = value;
+                first = false;
+            }
+        }
+
+        return maxValue;
     }
 
 }
